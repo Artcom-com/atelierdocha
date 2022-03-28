3c60de797f5f74dfcb93fd28eb28f52a3b43b31e
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-28 16:22:40 -0300

    feat: add document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          {/* <link rel="icon" href="/favicon.ico" /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
