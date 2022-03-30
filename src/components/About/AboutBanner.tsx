21d928bdf00ee26aeb4e61ff16df575a5441a1db
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-30 13:27:38 -0300

    feat: add about banner

---
+++ src/components/About/AboutBanner.tsx
@@ -0,0 +1,41 @@
+import { Flex, Text } from '@chakra-ui/react';
+import React from 'react';
+import FontBorder from '../UI/FontBorder/FontBorder';
+
+const AboutBanner = () => (
+  <Flex
+    bgImage="/images/banners/banner-two.jpg"
+    bgPosition="center"
+    bgRepeat="no-repeat"
+    bgSize="100% 100%"
+    w="100%"
+    h={{ base: '200px', md: '400px', xl: '600px' }}
+    alignItems="center"
+    px="1.5em"
+    my={{ base: '1.5em', xl: '2.5em' }}
+  >
+    <Flex
+      h="50%"
+    >
+      <Text
+        fontWeight="900"
+        fontSize={{ base: '2em', md: '4em', xl: '6em' }}
+        color="#fff"
+        lineHeight={1}
+        h="100%"
+      >
+        COMPARTILHE
+        {' '}
+        <br />
+        <FontBorder content="ENERGIA" />
+        {' '}
+        COM
+        <br />
+        {' '}
+        ATELIER DO CHÁ
+      </Text>
+    </Flex>
+  </Flex>
+);
+
+export default AboutBanner;
