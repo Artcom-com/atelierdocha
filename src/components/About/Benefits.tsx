65ed9a9ae64f65e3cc4eeb4fc49cd54545a835bb
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-30 14:17:01 -0300

    feat: add benefits reponsive display

--- src/components/About/Benefits.tsx
+++ src/components/About/Benefits.tsx
@@ -4,6 +4,8 @@ import {
 } from '@chakra-ui/react';
 import Image from 'next/image';
 
+import classes from './Benefits.module.css';
+
 const Benefits = (): JSX.Element => (
   <Flex
     flexDir="column"
@@ -34,50 +36,77 @@ const Benefits = (): JSX.Element => (
     </Box>
     <Flex
       w="100%"
-      justifyContent="space-between"
-      px="2em"
-      fontSize={{ base: '12px', md: '16px', xl: '20px' }}
+      justifyContent={{
+        base: 'flex-start', sm: 'flex-start', md: 'space-between', xl: 'space-between',
+      }}
+      flexDir={{
+        base: 'column', sm: 'column', md: 'row', xl: 'row',
+      }}
+      p="2em"
+      fontSize={{
+        base: '12px', sm: '14px', md: '16px', xl: '20px',
+      }}
+      gap={2}
+      bg={{ base: '#5b7300', sm: '#5b7300', md: '#fff' }}
+      color={{ base: '#fff', sm: '#fff', md: '#6b5f00' }}
     >
       <Text
-        w="16%"
-        textAlign="center"
+        w={{
+          base: '100%', sm: '100%', md: '16%', xl: '16%',
+        }}
+        textAlign={{ base: 'left', sm: 'left', md: 'center' }}
       >
+        ✔
         Auxílio no controle do
         equilíbrio do organismo
       </Text>
       <Text
-        w="16%"
-        textAlign="center"
+        w={{
+          base: '100%', sm: '100%', md: '16%', xl: '16%',
+        }}
+        textAlign={{ base: 'left', sm: 'left', md: 'center' }}
       >
-        Alivia a irritabilidade
+        ✔ Alivia a irritabilidade
+        <br className={classes.br} />
         e a insônia
       </Text>
       <Text
-        w="16%"
-        textAlign="center"
+        w={{
+          base: '100%', sm: '100%', md: '16%', xl: '16%',
+        }}
+        textAlign={{ base: 'left', sm: 'left', md: 'center' }}
       >
-        Diurético
+        ✔ Diurético
       </Text>
       <Text
-        w="16%"
-        textAlign="center"
+        w={{
+          base: '100%', sm: '100%', md: '16%', xl: '16%',
+        }}
+        textAlign={{ base: 'left', sm: 'left', md: 'center' }}
       >
-        Ativa o despertar
+        ✔ Ativa o despertar
+        {' '}
+        <br className={classes.br} />
         da mente e concentração
       </Text>
       <Text
-        w="16%"
-        textAlign="center"
+        w={{
+          base: '100%', sm: '100%', md: '16%', xl: '16%',
+        }}
+        textAlign={{ base: 'left', sm: 'left', md: 'center' }}
       >
-        Auxílio na desintoxicação
+        ✔ Auxílio na desintoxicação
+        <br className={classes.br} />
         do organismo e eliminar
         gordura
       </Text>
       <Text
-        w="16%"
-        textAlign="center"
+        w={{
+          base: '100%', sm: '100%', md: '16%', xl: '16%',
+        }}
+        textAlign={{ base: 'left', sm: 'left', md: 'center' }}
       >
-        Isotônico
+        ✔ Isotônico
         natural
       </Text>
     </Flex>
