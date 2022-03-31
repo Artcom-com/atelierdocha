1399fe4958ac85bf18a7b973d67d0a84010a29af
GustavoGomesDias <treeelevengg@gmail.com>
2022-03-31 16:17:07 -0300

    feat: init login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,11 @@ import PageHome from '../components/Home/Home';
 import About from '../components/About/About';
 import Products from '../components/Products/Products';
 import Footer from '../components/Layout/Footer/Footer';
+import SEO from '../components/SEO';
 
 const Home: NextPage = () => (
   <>
+    <SEO title="Atelier do Chá" description="Home page" />
     <Flex
       flexDir="column"
       px={{ base: '2em', xl: '4em' }}
