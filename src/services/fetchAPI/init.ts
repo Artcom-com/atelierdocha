1bd44c9a3867ec21cdf4823a43c2ac5bd649a88b
GustavoGomesDias <treeelevengg@gmail.com>
2022-04-18 13:49:15 -0300

    feat: change api url

--- src/services/fetchAPI/init.ts
+++ src/services/fetchAPI/init.ts
@@ -3,7 +3,7 @@ import { HttpResponse } from '../../../backend/helpers/http';
 
 const createAPI = (): FetchAPI<HttpResponse> => {
   if (process.env.NODE_ENV === 'production') {
-    return new FetchAPI('https://atelierdocha.vercel.app/api');
+    return new FetchAPI('https://atelierdocha.com/api');
   }
 
   return new FetchAPI('http://localhost:3000/api');
