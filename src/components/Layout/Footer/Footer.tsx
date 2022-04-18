a89db26aa4c6ab851f6cfa3c5f5821f02abb0cd6
GustavoGomesDias <treeelevengg@gmail.com>
2022-04-18 15:12:29 -0300

    feat: remove artcom website link

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -240,16 +240,7 @@ const Footer = (): JSX.Element => {
         w="full"
       >
         <Text color="#fff" textAlign="center" w="100%">
-          2022 - Atelier do Chá. Todos os direitos reservados. Desenvolvido por
-          {' '}
-          <a
-            href="https://www.artcom.com.br/"
-            rel="noopener noreferrer"
-            target="_blank"
-            style={{ textDecoration: 'underline' }}
-          >
-            Artcom Comnunicações.
-          </a>
+          2022 - Atelier do Chá. Todos os direitos reservados. Desenvolvido por Atelier do Chá
         </Text>
       </Flex>
     </Flex>
